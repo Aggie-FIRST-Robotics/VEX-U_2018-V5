9becf511fe881749131ac94e6a1c0fa9661896a1
Brett Etter <bbetter9101@gmail.com>
2018-10-21 02:59:40 -0500

    Updated action to include no_temp version
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>

--- include/afr-vexu-lib/action.h
+++ include/afr-vexu-lib/action.h
@@ -5,17 +5,23 @@
 #include "commandable.h"
 
 namespace AFR::VexU{
+    class action_no_temp : public scheduled{
+    public:
+        explicit action_no_temp(const unsigned int& update_period_ms);
+    };
+
     template<typename DataType>
-    class action : public scheduled{
+    class action : public action_no_temp{
     protected:
         commandable<DataType>* commandable_;
 
     public:
-        explicit action(commandable<DataType>* commandable);
+        explicit action(const unsigned int& update_period_ms, commandable<DataType>* commandable);
     };
 
     template<typename DataType>
-    action<DataType>::action(commandable<DataType>* commandable) : commandable_(commandable){}
+    action<DataType>::action(const unsigned int& update_period_ms, commandable <DataType>* commandable)
+            : action_no_temp(update_period_ms), commandable_(commandable){}
 }
 
 #endif //VEX_U_2018_V5_ACTION_H
---
+++ src/afr-vexu-lib/action.cpp
@@ -0,0 +1,3 @@
+#include <afr-vexu-lib/action.h>
+
+AFR::VexU::action_no_temp::action_no_temp(const unsigned int& update_period_ms) : scheduled(update_period_ms){}
