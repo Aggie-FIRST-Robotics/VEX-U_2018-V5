3a3d20462174801922bff751ceb8cb791bd69ff0
Brett Etter <bbetter9101@gmail.com>
2018-11-01 21:02:07 -0500

    Updated action
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>

--- src/afr-vexu-lib/action.cpp
+++ src/afr-vexu-lib/action.cpp
@@ -1,4 +1,4 @@
 #include "afr-vexu-lib/action.h"
 
-AFR::VexU::action::action(const unsigned int &update_period_ms, AFR::VexU::commandable *commandable) : scheduled(
+AFR::VexU::action::action(const scheduled_update_t& update_period_ms, AFR::VexU::commandable& commandable) : scheduled(
         update_period_ms), commandable_(commandable) {}
