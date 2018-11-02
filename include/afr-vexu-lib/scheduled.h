db6e48a490ce201a107233eb153357250878cadb
Brett Etter <bbetter9101@gmail.com>
2018-11-01 21:24:03 -0500

    updated scheduled
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>

--- include/afr-vexu-lib/scheduled.h
+++ include/afr-vexu-lib/scheduled.h
@@ -5,10 +5,11 @@
 
 namespace AFR::VexU{
     typedef unsigned int scheduled_update_t;
+    typedef std::milli scheduled_res_t;
 
     class scheduled{
         std::chrono::steady_clock::time_point next_update_;
-        std::chrono::duration<unsigned int, std::milli> update_period_ms_;
+        std::chrono::duration<scheduled_update_t, scheduled_res_t> update_period_;
 
         virtual void update_private(const double& delta_seconds) = 0;
 
@@ -17,6 +18,11 @@ namespace AFR::VexU{
 
         void update();
     };
+
+    template<typename T, typename Unit>
+    scheduled_update_t convert_time(const T& amount){
+        return std::chrono::duration_cast<scheduled_update_t, scheduled_res_t>(std::chrono::duration<T, Unit>{amount});
+    }
 }
 
 #endif //VEX_U_2018_V5_SCHEDULED_H
--- src/afr-vexu-lib/scheduled.cpp
+++ src/afr-vexu-lib/scheduled.cpp
@@ -2,13 +2,13 @@
 
 using steady_clock_n = std::chrono::steady_clock;
 
-AFR::VexU::scheduled::scheduled(const unsigned int& update_period) : update_period_ms_(update_period),
+AFR::VexU::scheduled::scheduled(const unsigned int& update_period) : update_period_(update_period),
                                                                      next_update_(steady_clock_n::now()){}
 
 void AFR::VexU::scheduled::update(){
     if(steady_clock_n::now() >= next_update_){
         update_private(std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1>>>(
-                steady_clock_n::now() - next_update_ + update_period_ms_).count());
-        next_update_ = steady_clock_n::now() + update_period_ms_;
+                steady_clock_n::now() - next_update_ + update_period_).count());
+        next_update_ = steady_clock_n::now() + update_period_;
     }
 }
