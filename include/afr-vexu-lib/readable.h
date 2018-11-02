0fe20fd3362c836aeebbdc3f0d26ce8c11dca80f
Brett Etter <bbetter9101@gmail.com>
2018-11-01 21:18:24 -0500

    updated readable
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>

--- include/afr-vexu-lib/readable.h
+++ include/afr-vexu-lib/readable.h
@@ -1,8 +1,9 @@
 #ifndef VEX_U_2018_V5_READABLE_H
 #define VEX_U_2018_V5_READABLE_H
 
-#include "scheduled.h"
 #include <any>
+
+#include "scheduled.h"
 
 namespace AFR::VexU{
     class readable : public scheduled{
@@ -10,7 +11,7 @@ namespace AFR::VexU{
         std::any value;
 
     public:
-        readable(const unsigned int& update_period_ms, const std::any& initial_value);
+        readable(const scheduled_update_t& update_period_ms, const std::any& initial_value);
 
         std::any get_value() const;
     };
--- src/afr-vexu-lib/readable.cpp
+++ src/afr-vexu-lib/readable.cpp
@@ -1,10 +1,6 @@
-//
-// Created by Buzzec on 10/26/2018.
-//
-
 #include <afr-vexu-lib/readable.h>
 
-AFR::VexU::readable::readable(const unsigned int& update_period_ms, const std::any& initial_value)
+AFR::VexU::readable::readable(const scheduled_update_t& update_period_ms, const std::any& initial_value)
         : scheduled(update_period_ms), value(initial_value){}
 
 std::any AFR::VexU::readable::get_value() const{
