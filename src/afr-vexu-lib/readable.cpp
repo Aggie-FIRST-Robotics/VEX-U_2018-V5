49f0db84b8c94fdd7ee55e99d8669277ed3f9559
Brett Etter <bbetter9101@gmail.com>
2018-11-08 16:36:33 -0600

    Updated Readable
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>

--- src/afr-vexu-lib/readable.cpp
+++ src/afr-vexu-lib/readable.cpp
@@ -1,9 +1,14 @@
 #include <afr-vexu-lib/readable.h>
 
-AFR::VexU::readable::readable(const scheduled_update_t& update_period, const std::any& initial_value)
-        : scheduled(update_period), value(initial_value){}
+AFR::VexU::readable::readable(const scheduled_update_t& update_period, const std::any& initial_value, error_t* result)
+        : scheduled(update_period), value(initial_value){
+    if(result != nullptr){
+        *result = SUCCESS;
+    }
+}
 
-std::any AFR::VexU::readable::get_value() const{
-    return value;
+AFR::VexU::error_t AFR::VexU::readable::get_value(std::any& result) const{
+    result = value;
+    return SUCCESS;
 }
 
