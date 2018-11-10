4be3cd7fb4388571e0b22c74a74cad8e0f7207e5
roflmaoqwertyqaz <tkdduo@gmail.com>
2018-11-09 22:26:06 -0600

    Get upstream updates (#3)
    * Changed result_t from bool to enum and renamed to error_t
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Updated scheduled
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Updated action
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Updated commandable
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Updated Readable
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Added call macro to defines
    
    Signed-off-by: Brett Etter <bbetter9101@gmail.com>
    
    * Updated ordered_input
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Updated scheduled
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Updated State
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Updated state_controller
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Updated base actions
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Fixed api_test
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>
    
    * Made scheduled return a reference
    
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- include/afr-vexu-lib/base_action/set_value_action.h
+++ include/afr-vexu-lib/base_action/set_value_action.h
@@ -7,15 +7,15 @@ namespace AFR::VexU::BaseAction{
     template<typename T>
     class set_value_action : public action{
         const T value_;
-        void update_private(const double& delta_seconds) override;
+        error_t update_private(const double& delta_seconds) override;
 
     public:
         set_value_action(const scheduled_update_t& update_period, commandable& commandable, const T& value);
     };
 
     template<typename T>
-    void set_value_action<T>::update_private(const double& delta_seconds){
-        commandable_.set_value(std::any{value_});
+    error_t set_value_action<T>::update_private(const double& delta_seconds){
+        return commandable_.set_value(std::any{value_});
     }
 
     template<typename T>
--- include/afr-vexu-lib/defines.h
+++ include/afr-vexu-lib/defines.h
@@ -1,6 +1,43 @@
-#define SUCCESS true
-#define FAIL false
+#ifndef VEX_U_2018_V5_DEFINES_H
+#define VEX_U_2018_V5_DEFINES_H
+
+#include <iostream>
+
+#ifdef AFR_VEXU_NODEBUG
+#define AFR_VEXU_CALL(x)
+#else
+#ifdef AFR_VEXU_TEST
+#ifdef AFR_VEXU_THROW_ON_ERROR
+#define AFR_VEXU_CALL(x)                                                                        \
+                if((x) != SUCCESS){                                                             \
+                    throw std::runtime_error{"Error encountered: " + std::to_string(error)};    \
+                }
+#else
+#define AFR_VEXU_CALL(x)                                                                        \
+                if((x) != SUCCESS){                                                             \
+                    std::cerr << "error: " << error << std::endl;                               \
+                }
+#endif
+#else
+#define AFR_VEXU_CALL                                                                           \
+            if((x) != SUCCESS){                                                                 \
+                                                                                                \
+            }
+#endif
+#endif
+
+#define AFR_VEXU_INTERNAL_CALL(x)                                       \
+    error_t internal_error_num_do_not_use = (x);                        \
+    if(internal_error_num_do_not_use != SUCCESS){                       \
+        return internal_error_num_do_not_use;                           \
+    }
 
 namespace AFR::VexU{
-    typedef bool result_t;
+    enum error_t{
+        SUCCESS,
+        GENERIC_FAILURE,
+        INVALID_VALUE,      //Commandable assigned invalid value
+    };
 }
+
+#endif //VEX_U_2018_V5_DEFINES_H
