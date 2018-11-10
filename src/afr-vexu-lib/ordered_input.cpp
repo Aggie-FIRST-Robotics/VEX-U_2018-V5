59c5c2fcefc1368ff49d6f964c72fbf23d49667a
Buzzec <bbetter9101@gmail.com>
2018-11-09 19:14:00 -0600

    Updated ordered_input
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- src/afr-vexu-lib/ordered_input.cpp
+++ src/afr-vexu-lib/ordered_input.cpp
@@ -1,12 +1,18 @@
 #include <afr-vexu-lib/ordered_input.h>
 
-AFR::VexU::ordered_input::ordered_input(const order_t& order, readable& input)
-        : order(order), input(input){}
-
-unsigned int AFR::VexU::ordered_input::get_order() const{
-    return order;
+AFR::VexU::ordered_input::ordered_input(const order_t& order, readable* input, error_t* result)
+        : order(order), input(input){
+    if(result != nullptr){
+        *result = SUCCESS;
+    }
 }
 
-AFR::VexU::readable& AFR::VexU::ordered_input::get_input() const{
-    return input;
+AFR::VexU::error_t AFR::VexU::ordered_input::get_order(order_t& result) const{
+    result = order;
+    return SUCCESS;
+}
+
+AFR::VexU::error_t AFR::VexU::ordered_input::get_input(readable*& result) const{
+    result = input;
+    return SUCCESS;
 }
