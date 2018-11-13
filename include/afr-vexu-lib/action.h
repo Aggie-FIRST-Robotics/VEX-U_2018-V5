28816a77e4dbaa388f34659b924a6a8e8837fc8a
Buzzec <bbetter9101@gmail.com>
2018-11-13 13:04:10 -0600

    Documented action
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- include/afr-vexu-lib/action.h
+++ include/afr-vexu-lib/action.h
@@ -9,9 +9,15 @@
 namespace AFR::VexU{
     class action : public scheduled {
     protected:
-        commandable& commandable_;
+        commandable& commandable_;  ///Reference to the commandable this action commands
 
     public:
+        /**
+         * Creates an action
+         * @param update_period the time period to be updated in of type scheduled_update_t
+         * @param commandable the commandable this action commands
+         * @param result error_t value if error encountered
+         */
         explicit action(const scheduled_update_t& update_period, commandable& commandable, error_t* result = nullptr);
     };
 }
