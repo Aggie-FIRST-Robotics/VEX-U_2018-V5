c76dcea6966ea1f31737b119991baae42a8e7f51
Buzzec <bbetter9101@gmail.com>
2018-11-13 14:59:14 -0600

    Documented set value action
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- include/afr-vexu-lib/base-action/set_value_action.h
+++ include/afr-vexu-lib/base-action/set_value_action.h
@@ -4,13 +4,25 @@
 #include "afr-vexu-lib/action.h"
 
 namespace AFR::VexU::BaseAction{
+    /**
+     * An action that sets a commandable to a single value
+     * @tparam T the type of constant value
+     */
     template<typename T>
     class set_value_action : public action{
         const T value_;
         error_t update_private(const double& delta_seconds) override;
 
     public:
-        set_value_action(const scheduled_update_t& update_period, commandable& commandable, const T& value);
+        /**
+         * Creates a set value action
+         * @param update_period passed to scheduled
+         * @param commandable the commandable to set
+         * @param value the constant value to be set
+         * @param result error_t value if error encountered
+         */
+        set_value_action(const scheduled_update_t& update_period, commandable& commandable, const T& value,
+                         error_t* result = nullptr);
     };
 
     template<typename T>
@@ -18,9 +30,14 @@ namespace AFR::VexU::BaseAction{
         return commandable_.set_value(std::any{value_});
     }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "readability-non-const-parameter"
     template<typename T>
     set_value_action<T>::set_value_action(const scheduled_update_t& update_period, commandable& commandable,
-                                          const T& value) : action(update_period, commandable), value_(value){}
+                                          const T& value, error_t* result) : action(update_period, commandable, result),
+                                                                             value_(value){}
+
+#pragma clang diagnostic pop
 }
 
 #endif //VEX_U_2018_V5_SET_VALUE_H
