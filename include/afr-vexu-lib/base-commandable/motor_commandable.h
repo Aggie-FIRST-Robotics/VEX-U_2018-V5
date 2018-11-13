0321d7ee654454a6e8888adcb89702c5793613d0
Buzzec <bbetter9101@gmail.com>
2018-11-13 14:46:41 -0600

    Documented motor commandable
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- include/afr-vexu-lib/base-commandable/motor_commandable.h
+++ include/afr-vexu-lib/base-commandable/motor_commandable.h
@@ -6,6 +6,12 @@
 #include "afr-vexu-lib/commandable.h"
 
 namespace AFR::VexU::BaseCommandable{
+    /**
+     * Represents a basic motor.
+     * <p>
+     * Value must be [-12000, 12000] and is in mV.
+     * </p>
+     */
     class motor_commandable : public commandable{
         pros::Motor motor;
 
@@ -13,9 +19,22 @@ namespace AFR::VexU::BaseCommandable{
         error_t check_value_private(const std::any& value) override;
 
     public:
+        /**
+         * Creates a basic motor commandable
+         * @param port v5 port to use
+         * @param gearset gearset attached to motor
+         * @param reverse should be reversed
+         * @param brake_mode what to do when power is 0
+         * @param result error_t value if error encountered
+         */
         motor_commandable(const std::uint8_t& port, const pros::motor_gearset_e_t& gearset, const bool& reverse,
                           const pros::motor_brake_mode_e_t& brake_mode, error_t* result);
 
+        /**
+         * Type is int16_t
+         * @param result int16_t
+         * @return error_t value if error encountered
+         */
         error_t get_type(std::type_index& result) const override;
     };
 }
