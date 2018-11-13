ed3171d50cd6d1ac4074992df9adfc712e80ff0f
Buzzec <bbetter9101@gmail.com>
2018-11-13 15:04:01 -0600

    Documented equivalent value action
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

--- include/afr-vexu-lib/base-action/equivalent_value_action.h
+++ include/afr-vexu-lib/base-action/equivalent_value_action.h
@@ -4,13 +4,25 @@
 #include "afr-vexu-lib/action.h"
 
 namespace AFR::VexU::BaseAction{
+    /**
+     * Action for equating a commandable to a value, represented by a pointer
+     * @tparam T the type of value
+     */
     template<typename T>
     class equivalent_value_action : public action{
         const T* value_ptr_;
         error_t update_private(const double& delta_seconds) override;
 
     public:
-        equivalent_value_action(const scheduled_update_t& update_period, commandable& commandable, const T* value_ptr);
+        /**
+         * Creates a equivalent value action
+         * @param update_period passed to scheduled
+         * @param commandable passed to action
+         * @param value_ptr the pointer to the value to be copied every update
+         * @param result error_t value if error encountered
+         */
+        equivalent_value_action(const scheduled_update_t& update_period, commandable& commandable, const T* value_ptr,
+                                error_t* result = nullptr);
     };
 
     template<typename T>
@@ -18,11 +30,16 @@ namespace AFR::VexU::BaseAction{
         return commandable_.set_value(std::any{*value_ptr_});
     }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "readability-non-const-parameter"
     template<typename T>
     equivalent_value_action<T>::equivalent_value_action(const scheduled_update_t& update_period,
-                                                        commandable& commandable, const T*) : action(update_period,
-                                                                                                     commandable),
-                                                                                              value_ptr_(value_ptr_){}
+                                                        commandable& commandable, const T* value_ptr, error_t* result)
+            : action(update_period,
+                     commandable, result),
+              value_ptr_(value_ptr){}
+
+#pragma clang diagnostic pop
 }
 
 #endif //VEX_U_2018_V5_EQUIVALENT_VALUE_ACTION_H
