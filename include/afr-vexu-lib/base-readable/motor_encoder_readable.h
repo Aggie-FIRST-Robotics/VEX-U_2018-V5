c957b391b2a83432a4dec55b57c82147a0c48916
Buzzec <bbetter9101@gmail.com>
2018-11-16 21:59:12 -0600

    Added motor encoder
    Signed-off-by: Buzzec <bbetter9101@gmail.com>

---
+++ include/afr-vexu-lib/base-readable/motor_encoder_readable.h
@@ -0,0 +1,32 @@
+#ifndef VEX_U_2018_V5_MOTOR_ENCODER_READABLE_H
+#define VEX_U_2018_V5_MOTOR_ENCODER_READABLE_H
+
+#include "main.h"
+
+#include "afr-vexu-lib/readable.h"
+
+namespace AFR::VexU::BaseReadable{
+    class motor_encoder_readable : public readable{
+        pros::Motor motor;
+        double velocity;
+        double position;
+        double scale_factor;
+
+        error_t update_private(const double& delta_seconds) override;
+
+    public:
+        motor_encoder_readable(const std::uint8_t& port, const pros::motor_gearset_e_t& gearset, const bool& reverse,
+                               const double& scale_factor, const scheduled_update_t& update_period, error_t* result);
+
+        error_t set_scale_factor(const double& scale_factor);
+        error_t get_actual_velocity(double& result);
+        error_t get_scaled_velocity(double& result);
+        error_t get_position(double& result);
+        error_t get_scaled_position(double& result);
+        error_t tare_position();
+        error_t tare_position(const double& position);
+        error_t tare_position_scaled(const double& position);
+    };
+}
+
+#endif //VEX_U_2018_V5_MOTOR_ENCODER_READABLE_H
---
+++ src/afr-vexu-lib/base-readable/motor_encoder_readable.cpp
@@ -0,0 +1,64 @@
+#include "afr-vexu-lib/base-readable/motor_encoder_readable.h"
+
+namespace AFR::VexU::BaseReadable{
+    error_t motor_encoder_readable::update_private(const double& delta_seconds){
+        double temp_velocity = motor.get_actual_velocity();
+        AFR_PROS_INTERNAL_CALL(temp_velocity, PROS_ERR_F);
+        velocity = temp_velocity;
+
+        double temp_position = motor.get_position();
+        AFR_PROS_INTERNAL_CALL(temp_position, PROS_ERR_F);
+        position = temp_position;
+
+        return SUCCESS;
+    }
+
+    motor_encoder_readable::motor_encoder_readable(const std::uint8_t& port, const pros::motor_gearset_e_t& gearset,
+                                                   const bool& reverse, const double& scale_factor,
+                                                   const scheduled_update_t& update_period,
+                                                   AFR::VexU::error_t* result) : readable(update_period, 0, result),
+                                                                                 motor(port, gearset, reverse,
+                                                                                       pros::E_MOTOR_ENCODER_COUNTS),
+                                                                                 velocity(0), position(0),
+                                                                                 scale_factor(scale_factor){}
+
+    error_t motor_encoder_readable::set_scale_factor(const double& scale_factor){
+        this->scale_factor = scale_factor;
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::get_actual_velocity(double& result){
+        result = velocity;
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::get_scaled_velocity(double& result){
+        result = velocity * scale_factor;
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::get_position(double& result){
+        result = position * scale_factor;
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::get_scaled_position(double& result){
+        result = position * scale_factor;
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::tare_position(){
+        AFR_PROS_INTERNAL_CALL(motor.tare_position(), PROS_ERR);
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::tare_position(const double& position){
+        AFR_PROS_INTERNAL_CALL(motor.set_zero_position(position), PROS_ERR);
+        return SUCCESS;
+    }
+
+    error_t motor_encoder_readable::tare_position_scaled(const double& position){
+        AFR_PROS_INTERNAL_CALL(motor.set_zero_position(position / scale_factor), PROS_ERR);
+        return SUCCESS;
+    }
+}
