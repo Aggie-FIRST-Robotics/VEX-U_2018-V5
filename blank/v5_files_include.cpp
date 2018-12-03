247ab164617bde534c9772d88da57e03357e4ce4
Aamhish Rao <aamhish@gmail.com>
2018-12-02 18:32:27 -0600

    this is too much
    Signed-off-by: Aamhish Rao <aamhish@gmail.com>

---
+++ blank/v5_files_include.cpp
@@ -0,0 +1,5 @@
+#include "afr-vexu-lib/base-commandable/motor_commandable.h"
+#include "afr-vexu-lib/base-commandable/adi_analog_commandable.h"
+#include "afr-vexu-lib/base-readable/adi_encoder_readable.h"
+#include "afr-vexu-lib/base-readable/motor_encoder_readable.h"
+#include "afr-vexu-lib/base-readable/adi_analog_readable.h"
