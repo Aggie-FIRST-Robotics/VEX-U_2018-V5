a285f5a6cfc38904d02b26ddbc153befa62445cf
Aamhish Rao <aamhish@gmail.com>
2018-12-02 20:58:17 -0600

    Fix merge conflicts

--- include/afr-vexu-lib/base-commandable/adi_digital_commandable.h
+++ include/afr-vexu-lib/base-commandable/adi_digital_commandable.h
@@ -1,4 +1,3 @@
-
 #ifndef VEX_U_2018_V5_ADI_DIGITAL_COMMANDABLE_H
 #define VEX_U_2018_V5_ADI_DIGITAL_COMMANDABLE_H
 
@@ -8,11 +7,17 @@
 
 namespace AFR::VexU::BaseCommandable {
     class adi_digital_commandable : public commandable {
+        pros::ADIDigitalOut adi_digital_out;
 
+        error_t set_value_private(const std::any &value) override;
 
+        error_t check_value_private(const std::any &value) override;
+
+    public:
+        explicit adi_digital_commandable(const uint8_t &port, error_t *result = nullptr);
+
+        error_t get_type(std::type_index &result) const override;
     };
-
 }
-
 
 #endif //VEX_U_2018_V5_ADI_DIGITAL_COMMANDABLE_H
