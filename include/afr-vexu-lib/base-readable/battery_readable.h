9ad8dd9eea9b7069e366e6fd250c6d875da344b7
Christian Pawlowicz <christian.pawlowicz@gmail.com>
2018-12-02 20:46:13 -0600

    Battery Readable Header/Source

--- include/afr-vexu-lib/base-readable/battery_readable.h
+++ include/afr-vexu-lib/base-readable/battery_readable.h
@@ -17,17 +17,47 @@ namespace AFR::VexU::BaseReadable {
         double temperature;
         double voltage;
 
+        /**
+         * Overrides scheduled, updates private period
+         * @param delta_seconds new update period
+         * @return error_t value if error encountered
+         */
         error_t update_private(const double &delta_seconds) override;
 
     public:
+        /**
+         * Creates a battery readable
+         * @param update_period the update period for the readable
+         * @param result error_t value if error encountered
+         */
         battery_readable(const scheduled_update_t &update_period, error_t *result);
 
+        /**
+         * Returns battery capacity
+         * @param result battery capacity
+         * @return error_t value if error encountered
+         */
         error_t get_capacity(double &result);
 
+        /**
+         * Returns battery current
+         * @param result battery current
+         * @return error_t value if error encountered
+         */
         error_t get_current(double &result);
 
+        /**
+         * Returns battery temperature
+         * @param result battery temperature
+         * @return error_t value if error encountered
+         */
         error_t get_temperature(double &result);
 
+        /**
+         * Returns battery voltage
+         * @param result battery voltage
+         * @return error_t value if error encountered
+         */
         error_t get_voltage(double &result);
     };
 
--- src/afr-vexu-lib/base-readable/battery_readable.cpp
+++ src/afr-vexu-lib/base-readable/battery_readable.cpp
@@ -1,6 +1,11 @@
 #include "afr-vexu-lib/base-readable/battery_readable.h"
 
 namespace AFR::VexU::BaseReadable {
+    /**
+     * Overrides scheduled, updates private variables and period
+     * @param delta_seconds new update period
+     * @return error_t value if error encountered
+     */
     error_t battery_readable::update_private(const double &delta_seconds) {
         double temp_capacity = prosbattery.get_capacity();
         AFR_PROS_INTERNAL_CALL(temp_capacity, PROS_ERR_F);
@@ -21,6 +26,11 @@ namespace AFR::VexU::BaseReadable {
         return SUCCESS;
     }
 
+    /**
+     * Creates a battery readable
+     * @param update_period the update period for the readable
+     * @param result error_t value if error encountered
+     */
     battery_readable::battery_readable(const scheduled_update_t &update_period, AFR::VexU::error_t *result)
             : readable(update_period, 0, result),
               capacity(prosbattery.get_capacity()),
@@ -29,22 +39,41 @@ namespace AFR::VexU::BaseReadable {
                       prosbattery.get_temperature()),
               voltage(prosbattery.get_voltage()) {}
 
-
+    /**
+     * Returns battery capacity
+     * @param result battery capacity
+     * @return error_t value if error encountered
+     */
     error_t battery_readable::get_capacity(double &result) {
         result = capacity;
         return SUCCESS;
     }
 
+    /**
+     * Returns battery current
+     * @param result battery current
+     * @return error_t value if error encountered
+     */
     error_t battery_readable::get_current(double &result) {
         result = current;
         return SUCCESS;
     }
 
+    /**
+     * Returns battery temperature
+     * @param result battery temperature
+     * @return error_t value if error encountered
+     */
     error_t battery_readable::get_temperature(double &result) {
         result = temperature;
         return SUCCESS;
     }
 
+    /**
+     * Returns battery voltage
+     * @param result battery voltage
+     * @return error_t value if error encountered
+     */
     error_t battery_readable::get_voltage(double &result) {
         result = voltage;
         return SUCCESS;
