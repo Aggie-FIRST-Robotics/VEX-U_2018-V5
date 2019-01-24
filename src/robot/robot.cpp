e988f7edf81ddf5b4f8b7a6498fcab6337e7b49c
buzzec <bbetter9101@gmail.com>
2019-01-23 20:04:31 -0600

    Added robot file
    Signed-off-by: buzzec <bbetter9101@gmail.com>

---
+++ src/robot/robot.cpp
@@ -0,0 +1,23 @@
+
+
+namespace AFR::VexU{
+    void init_robot(){
+
+    }
+
+    void competion_init(){
+
+    }
+
+    void auto_robot(){
+
+    }
+
+    void opcontrol_robot(){
+
+    }
+
+    void disabled_robot(){
+
+    }
+}
